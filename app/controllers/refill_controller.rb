ab959121bda9a448130d6be5efcfba2ccb8cbf38
Patrick McGrath <patrickmcgrath@Patricks-MacBook-Pro.local>
2020-01-10 20:33:21 -0500

    Adding some login and logout buttons to the header, manadate authentication on the refill controller

--- app/controllers/refill_controller.rb
+++ app/controllers/refill_controller.rb
@@ -1,4 +1,6 @@
 class RefillController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :refill only: [:index, :create]
+  resources :refill, only: [:index, :create]
   root "home#index"
 end
