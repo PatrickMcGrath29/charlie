840e5172da06184245465c8f953ab72c619d6d53
Patrick McGrath <patrickmcgrath@Patricks-MacBook-Pro.local>
2020-01-16 15:09:04 -0500

    Updating devise to include support for additional parameters, view updates, working login / selecting charlie card

--- app/controllers/refills_controller.rb
+++ app/controllers/refills_controller.rb
@@ -6,12 +6,14 @@ class RefillsController < ApplicationController
   end
 
   def create
+    puts "here"
     @refill = Refill.new(refill_create_params)
+    @refill.execute
   end
 
   private
 
   def refill_create_params
-    params.require(:refill).permit(:security_code, :refill_amount, :charlie_card_number, :use_default_charlie_card)
+    params.require(:refill).permit(:security_code, :refill_amount, :charlie_card_number, :use_default_charlie_card).merge(user: current_user)
   end
 end
--- app/models/refill.rb
+++ app/models/refill.rb
@@ -2,9 +2,12 @@ class Refill < ApplicationRecord
   belongs_to :user
   attr_accessor :current_user
 
-  def initialize(current_user)
+  def initialize(params)
     super
-    current_user = current_user
+
+    @current_user = params[:user]
+    chromedriver_path = File.join(File.absolute_path(File.dirname(__FILE__)), "chromedriver")
+    Selenium::WebDriver::Chrome.driver_path = chromedriver_path
 
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
@@ -17,29 +20,29 @@ class Refill < ApplicationRecord
 
   def execute
     # navigate to start page
-    driver.navigate.to 'https://charliecard.mbta.com/CharlieCardWebProgram/pages/charlieCardCenter.jsf'
+    @driver.navigate.to 'https://charliecard.mbta.com/CharlieCardWebProgram/pages/charlieCardCenter.jsf'
     # go to login form
-    driver.find_element(:id, 'main_form:_idJsp70').click
-    username = driver.find_element(:id, 'main_form:Email')
-    password = driver.find_element(:id, 'main_form:Password')
+    @driver.find_element(:id, 'main_form:_idJsp70').click
+    username = @driver.find_element(:id, 'main_form:Email')
+    password = @driver.find_element(:id, 'main_form:Password')
 
-    # add creds for login
+    # # add creds for login
     if username && password
-      username.send_keys(current_user.email)
-      password.send_keys("123")
+      username.send_keys(@current_user.email)
+      password.send_keys(@current_user.charlie_card_password)
     end
 
-    # submit login form
-    driver.find_element(:id, 'main_form:_idJsp78').click
+    # # submit login form
+    @driver.find_element(:id, 'main_form:_idJsp78').click
 
-    # # select add stored value button
-    driver.find_element(:id, 'contentMenu_form:_idJsp68').click
-    charlie_cards = driver.find_elements(:css, 'select.listBox option')
+    # # # select add stored value button
+    @driver.find_element(:id, 'contentMenu_form:_idJsp68').click
+    charlie_cards = @driver.find_elements(:css, 'select.listBox option')
     charlie_cards[0].click
 
     # resize the window and take a screenshot
-    driver.manage.window.resize_to(800, 800)
-    driver.save_screenshot 'intoli-screenshot.png'
+    @driver.manage.window.resize_to(800, 800)
+    @driver.save_screenshot 'test-screenshot.png'
   end
 
 end
